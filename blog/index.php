e23d74d9bf03e225dac369d3cb7973a1774d47a6
Reyhan tok <120533118+Herandzeus@users.noreply.github.com>
2023-06-20 13:34:11 +0300

    Add files via upload

---
+++ blog/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Ana Sayfa</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <header>
+    <h1>Hoş Geldiniz!</h1>
+  </header>
+
+  <nav>
+    <ul>
+      <li><a href="index.php">Anasayfa</a></li>
+      <li><a href="Books.php">Kitaplarımız</a></li>
+      <li><a href="Movie.php">Filmlerimiz</a></li>
+      <li><a href="contact.php">İletişim</a></li>
+    </ul>
+  </nav>
+
+  <div class="panel">
+    <div class="panel-item">
+      <h2>Anasayfa</h2>
+      <!-- Anasayfa içeriği buraya gelebilir -->
+    </div>
+
+    <div class="panel-item">
+      <h2>Kitaplarımız</h2>
+      <!-- Kitaplarımız içeriği buraya gelebilir -->
+    </div>
+
+    <div class="panel-item">
+      <h2>Filmlerimiz</h2>
+      <!-- Filmlerimiz içeriği buraya gelebilir -->
+    </div>
+
+    <div class="panel-item">
+      <h2>İletişim</h2>
+      <!-- İletişim içeriği buraya gelebilir -->
+    </div>
+  </div>
+
+  <footer>
+    <p>Telif Hakkı &copy; 2023 Şirket Adı</p>
+  </footer>
+</body>
+</html>
