76d61e3d006c7ac6285606453bb7c25bf2af38f8
Reyhan tok <120533118+Herandzeus@users.noreply.github.com>
2023-06-22 01:26:43 +0300

    Add files via upload

--- blog/index.php
+++ blog/index.php
@@ -3,45 +3,67 @@
 <head>
   <title>Ana Sayfa</title>
   <link rel="stylesheet" href="styles.css">
+  <style>
+
+    html, body {
+      height: 100%;
+    }
+
+    video {
+      position: fixed;
+      top: 0;
+      left: 0;
+      min-width: 100%;
+      min-height: 100%;
+      z-index: -1;
+    }
+  </style>
 </head>
 <body>
-  <header>
-    <h1>Hoş Geldiniz!</h1>
-  </header>
+  <video autoplay loop muted>
+    <source src="video.mp4" type="video/mp4">
+    
+  </video>
 
-  <nav>
-    <ul>
-      <li><a href="index.php">Anasayfa</a></li>
-      <li><a href="Books.php">Kitaplarımız</a></li>
-      <li><a href="Movie.php">Filmlerimiz</a></li>
-      <li><a href="contact.php">İletişim</a></li>
-    </ul>
-  </nav>
-
-  <div class="panel">
-    <div class="panel-item">
-      <h2>Anasayfa</h2>
-      <!-- Anasayfa içeriği buraya gelebilir -->
-    </div>
-
+  <nav class="panel">
     <div class="panel-item">
       <h2>Kitaplarımız</h2>
-      <!-- Kitaplarımız içeriği buraya gelebilir -->
+      <ul class="dropdown-menu">
+        <li><a href="#">Roman</a></li>
+        <li><a href="#">Dünya Klasikleri</a></li>
+        <li><a href="#">Edebiyat</a></li>
+        <li><a href="#">Şiir</a></li>
+      </ul>
     </div>
 
     <div class="panel-item">
       <h2>Filmlerimiz</h2>
-      <!-- Filmlerimiz içeriği buraya gelebilir -->
+      <ul class = "dropdown-menu">
+        <li><a href="#">Korku</a></li>
+        <li><a href="#">Komedi</a></li>
+        <li><a href="#">Aksiyon</a></li>
+        <li><a href="#">Bilim Kurgu</a></li>
+        <li><a href="#">Dram</a></li>
+        <li><a href="#">Romantik</a></li>
+        <li><a href="#">Belgesel</a></li>
+        <li><a href="#">Macera</a></li>
+        <li><a href="#">Gerilim</a></li>
+      </ul>
     </div>
 
     <div class="panel-item">
       <h2>İletişim</h2>
-      <!-- İletişim içeriği buraya gelebilir -->
+      <ul class="dropdown-menu">
+        <li><a href="contact.php">İletişim</a></li>
+        <li><a href="about.php">Hakkımızda</a></li>
+      </ul>
     </div>
-  </div>
+  </nav>
 
-  <footer>
-    <p>Telif Hakkı &copy; 2023 Şirket Adı</p>
-  </footer>
+  <h1 style="text-align: center;">Benim Dünyam</h1>
+
+  <p style="text-align: center; font-family: 'Times New Roman', Times, serif; font-size: 20px;">Kitap ve film önerileri için doğru adrestesiniz.</p>
+  <p style="text-align: center; font-family: 'Times New Roman', Times, serif; font-size: 20px;">Sitemizdeki kitap ve filmler hakkında yorum yapabilir, puan verebilirsiniz.</p>
+  
 </body>
 </html>
