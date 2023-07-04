682cb28afd5c670ff3fbe4470e81bbec213130c4
yakuza <reyhantkk@gmail.com>
2023-07-05 01:33:11 +0300

    update

--- blog/index.php
+++ blog/index.php
@@ -2,9 +2,8 @@
 <html>
 <head>
   <title>Ana Sayfa</title>
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <style>
-
     html, body {
       height: 100%;
     }
@@ -22,48 +21,67 @@
 <body>
   <video autoplay loop muted>
     <source src="video.mp4" type="video/mp4">
-    
   </video>
 
-  <nav class="panel">
-    <div class="panel-item">
-      <h2>Kitaplarımız</h2>
-      <ul class="dropdown-menu">
-        <li><a href="#">Roman</a></li>
-        <li><a href="#">Dünya Klasikleri</a></li>
-        <li><a href="#">Edebiyat</a></li>
-        <li><a href="#">Şiir</a></li>
-      </ul>
-    </div>
-
-    <div class="panel-item">
-      <h2>Filmlerimiz</h2>
-      <ul class = "dropdown-menu">
-        <li><a href="#">Korku</a></li>
-        <li><a href="#">Komedi</a></li>
-        <li><a href="#">Aksiyon</a></li>
-        <li><a href="#">Bilim Kurgu</a></li>
-        <li><a href="#">Dram</a></li>
-        <li><a href="#">Romantik</a></li>
-        <li><a href="#">Belgesel</a></li>
-        <li><a href="#">Macera</a></li>
-        <li><a href="#">Gerilim</a></li>
-      </ul>
-    </div>
-
-    <div class="panel-item">
-      <h2>İletişim</h2>
-      <ul class="dropdown-menu">
-        <li><a href="contact.php">İletişim</a></li>
-        <li><a href="about.php">Hakkımızda</a></li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Ana Sayfa</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="booksDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Kitaplarımız
+            </a>
+            <div class="dropdown-menu" aria-labelledby="booksDropdown">
+              <a class="dropdown-item" href="#">Roman</a>
+              <a class="dropdown-item" href="#">Dünya Klasikleri</a>
+              <a class="dropdown-item" href="#">Edebiyat</a>
+              <a class="dropdown-item" href="#">Şiir</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="moviesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Filmlerimiz
+            </a>
+            <div class="dropdown-menu" aria-labelledby="moviesDropdown">
+              <a class="dropdown-item" href="#">Korku</a>
+              <a class="dropdown-item" href="#">Komedi</a>
+              <a class="dropdown-item" href="#">Aksiyon</a>
+              <a class="dropdown-item" href="#">Bilim Kurgu</a>
+              <a class="dropdown-item" href="#">Dram</a>
+              <a class="dropdown-item" href="#">Romantik</a>
+              <a class="dropdown-item" href="#">Belgesel</a>
+              <a class="dropdown-item" href="#">Macera</a>
+              <a class="dropdown-item" href="#">Gerilim</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">İletişim</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">Hakkımızda</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 
-  <h1 style="text-align: center;">Benim Dünyam</h1>
+  <div class="container mt-5">
+    <h1 class="text-center">Benim Dünyam</h1>
+    <p class="text-center">Kitap ve film önerileri için doğru adrestesiniz.</p>
+    <p class="text-center">Sitemizdeki kitap ve filmler hakkında yorum yapabilir, puan verebilirsiniz.</p>
 
-  <p style="text-align: center; font-family: 'Times New Roman', Times, serif; font-size: 20px;">Kitap ve film önerileri için doğru adrestesiniz.</p>
-  <p style="text-align: center; font-family: 'Times New Roman', Times, serif; font-size: 20px;">Sitemizdeki kitap ve filmler hakkında yorum yapabilir, puan verebilirsiniz.</p>
-  
+    <div class="text-center">
+      <a href="login.php" class="btn btn-primary">Giriş Yap</a>
+      <a href="register.php" class="btn btn-secondary">Kayıt Ol</a>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
